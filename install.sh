709d94460ae8bf687696c179f267ebfa3561203a
Jorge Narvaez <jormelcn@gmail.com>
2019-06-15 18:08:48 -0500

    Funcionalidad basica

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sudo cp ./bin/ngx-http /usr/local/bin/ngx-http
+sudo cp ./bin/ngx-https /usr/local/bin/ngx-https
+sudo cp ./bin/ngx-certificate /usr/local/bin/ngx-certificate
+sudo cp ./bin/ngx-reload /usr/local/bin/ngx-reload
